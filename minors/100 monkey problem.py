6c0b545955ca5760640edd8f184ec9b5e324c301
satyam maddeshiya <141503174+the-satym@users.noreply.github.com>
2025-06-21 20:10:05 +0530

    Update 100 monkey problem.py

--- minors/100 monkey problem.py
+++ minors/100 monkey problem.py
@@ -3,6 +3,5 @@ monkey=(i for i in range (1, 101))
 for i in monkey:
     for j in range (i, 101, i):
         doors[j] = not doors[j]
-print(doors)
 open_doors = [door for door, state in doors.items() if state]
 print("Open doors:", open_doors)
